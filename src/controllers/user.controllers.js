614f10638b2f53c9635cf42a180b72999bfde7a9
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2023-12-26 13:19:43 -0700

    backend finished

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 const catchError = require('../utils/catchError');
 const User = require('../models/User');
+//const bcrypt = require('bcryptjs');
 
 const getAll = catchError(async(req, res) => {
     const users = await User.findAll();
@@ -17,9 +18,41 @@ const create = catchError(async(req, res) => {
     })
 
     return res.status(201).json(user);
+});
+
+const update = catchError(async(req,res) => {
+    const { id } = req.params;
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.update({
+        first_name,
+        last_name,
+        email,
+        password,
+        birthday    
+    }, { where: { id }, returning: true });
+    return res.json(user);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+
+    return res.json(user);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id }})
+
+    return res.sendStatus(204);
 })
+
+
 
 module.exports = {
     getAll,
     create,
+    update,
+    getOne,
+    remove
 }
